b9ff0db1581491f05c0a0c0339d31f701fd34889
Allen-Avanheim <86322722+Allen-Avanheim@users.noreply.github.com>
2022-07-14 21:13:50 +0300

    Release 1.0.1 - Improved app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from os import system
+from os import system, path
 
 
-def start_server():
-    system('python manage.py runserver')
+class App:
+    @staticmethod
+    def start():
+        App.__install_dependencies()
+        App.__run_server()
+
+    @staticmethod
+    def __install_dependencies():
+        try:
+            import django
+        except ModuleNotFoundError:
+            if path.isfile('requirements.txt'):
+                system('pip install -r requirements.txt')
+            else:
+                print("There are not enough dependencies to run the module.\n"
+                      "Please download the requirements.txt file from:\n"
+                      "https://github.com/Allen-Avanheim/softformance_4/blob/master/requirements.txt")
+                exit()
+
+    @staticmethod
+    def __run_server():
+        system('python manage.py runserver')
 
 
 if __name__ == "__main__":
-    start_server()
+    app = App()
+    app.start()
